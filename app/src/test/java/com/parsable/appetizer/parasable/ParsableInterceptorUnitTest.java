33a44a14ca2146d73c8c16d0004be292a3140e55
noobiehacker <davidkwokhochan@gmail.com>
2016-03-09 20:43:46 -0800

    Added Interceptor Test

--- app/src/test/java/com/parsable/appetizer/parasable/ParsableInterceptorUnitTest.java
+++ app/src/test/java/com/parsable/appetizer/parasable/ParsableInterceptorUnitTest.java
@@ -4,11 +4,11 @@ import com.parsable.appetizer.parasable.Network.ParsableInterceptor;
 
 import junit.framework.Assert;
 
-import org.bouncycastle.ocsp.Req;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.easymock.PowerMock;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.robolectric.RobolectricTestRunner;
@@ -23,7 +23,7 @@ import static org.mockito.Mockito.*;
  */
 @RunWith(PowerMockRunner.class)
 @Config(sdk = Build.VERSION_CODES.LOLLIPOP)
-@PrepareForTest({Request.class ,Response.class, Interceptor.Chain.class})
+@PrepareForTest({Request.class ,Request.Builder.class ,Response.class, Interceptor.Chain.class})
 public class ParsableInterceptorUnitTest {
 
     @Test
@@ -33,20 +33,21 @@ public class ParsableInterceptorUnitTest {
 
         //  create mock
         Interceptor.Chain chain = Mockito.mock(Interceptor.Chain.class);
-        Response expectedResponse = PowerMock.createMock(Response.class);
+        Response expectedResponse = PowerMockito.mock(Response.class);
         Response acturalResponse = null;
-        Request mockRequest = PowerMock.createMock(Request.class);
-
+        Request mockRequest = Mockito.mock(Request.class);
+        Request.Builder builder = Mockito.mock(Request.Builder.class);
         try{
+
+            doReturn(mockRequest).when(chain).request();
+            doReturn(builder).when(mockRequest).newBuilder();
+            //Some how this bypass the return type error where as doReturn fails
             when(chain.proceed(chain.request())).thenReturn(expectedResponse);
-            //Chain.request needs to return a new request
-            when(chain.request()).thenReturn(mockRequest);
-            //TODO: All the builder methods just return self
-//            .newBuilder()
-//                    .addHeader("deviceplatform", "android")
-//                    .removeHeader("User-Agent")
-//                    .addHeader("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0")
-//                    .build();
+            doReturn(builder).when(builder).addHeader("deviceplatform","android");
+            doReturn(builder).when(builder).addHeader("User-Agent"
+                    , "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0");
+            doReturn(builder).when(builder).removeHeader("User-Agent");
+            doReturn(mockRequest).when(builder).build();
             acturalResponse = interceptor.intercept(chain);
 
         }catch(Exception e){
